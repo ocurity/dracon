2b0c856fa93a68071182d51861164953c58b242b
spyros <spyros@ocurity.com>
2023-03-13 22:19:41 +0000

    utility that validates the e2e tests have ran and produced correct results

---
+++ test/ensure_hashes/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	v1 "github.com/ocurity/dracon/api/proto/v1"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	dbURL          string
+	dbName         string
+	collectionName string
+	hashesLoc      string
+)
+
+func init() {
+	flag.StringVar(&dbURL, "db-uri", "https://localhost:8529", "URI to connect to MongoDB.")
+	flag.StringVar(&dbName, "db-name", "consumer-mongodb", "The MongoDB database name to use.")
+	flag.StringVar(&collectionName, "collection-name", "consumer-mongodb", "The MongoDB collection name to use.")
+	flag.StringVar(&hashesLoc, "hashLoc", "/hashes.txt", "The path to the file containing the hashes that should be in the database")
+}
+
+func main() {
+	flag.Parse()
+
+	ctx := context.Background()
+	bytesRead, err := ioutil.ReadFile(hashesLoc)
+	if err != nil {
+		fmt.Println(err)
+		panic("Could not read hashes file")
+	}
+	fileContent := string(bytesRead)
+	lines := strings.Split(fileContent, "\n")
+
+	if err := run(ctx, lines); err != nil {
+		log.Fatalf("could not run: %s", err)
+	}
+}
+
+func run(ctx context.Context, want []string) error {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dbURL))
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	coll := client.Database(dbName).Collection(collectionName)
+
+	cur, err := coll.Find(ctx, bson.D{{"hash", 1}})
+	var issues []*v1.EnrichedIssue
+	if err := cur.All(ctx, &issues); err != nil {
+		return err
+	}
+	var hashes []string
+	for _, iss := range issues {
+		hashes = append(hashes, iss.GetHash())
+	}
+	if err != nil {
+		return err
+	}
+	if !compare(want, hashes) {
+		return fmt.Errorf("%s\n", "Lists are different, comparison failed")
+	}
+	return nil
+}
+
+func compare(want, have []string) bool {
+
+	if len(want) == 0 && len(have) == 0 {
+		return true
+	}
+	extraHave := []string{}
+	extraWant := []string{}
+	wantLen := len(want)
+	haveLen := len(have)
+
+	visited := make([]bool, haveLen)
+	for i := 0; i < wantLen; i++ {
+		found := false
+		for j := 0; j < haveLen; j++ {
+			if visited[j] {
+				continue
+			}
+			if want[i] == have[j] {
+				visited[j] = true
+				found = true
+				break
+			}
+		}
+		if !found {
+			extraWant = append(extraWant, want[i])
+		}
+	}
+
+	for j := 0; j < haveLen; j++ {
+		if visited[j] {
+			continue
+		}
+		extraHave = append(extraHave, have[j])
+	}
+
+	if len(extraWant) == 0 && len(extraHave) == 0 {
+		return true
+	}
+
+	fmt.Println("Hash lists are different want(len:", len(want), ")!=have(len:", len(have), ")", "want has the following elements different")
+	fmt.Println(extraWant)
+	fmt.Println("Have has the following extra elements")
+	fmt.Println(extraHave)
+
+	return false
+}
