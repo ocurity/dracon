1323ef6359e544968033b56f292961fb14c3d263
spyros <spyros@ocurity.com>
2023-04-30 21:17:51 +0100

    add enum transformers

---
+++ pkg/enumtransformers/enumtransformers.go
@@ -0,0 +1,39 @@
+package enumtransformers
+
+import (
+	v1 "github.com/ocurity/dracon/api/proto/v1"
+)
+
+func SeverityToText(severity v1.Severity) string {
+	switch severity {
+	case v1.Severity_SEVERITY_INFO:
+		return "Info"
+	case v1.Severity_SEVERITY_LOW:
+		return "Low"
+	case v1.Severity_SEVERITY_MEDIUM:
+		return "Medium"
+	case v1.Severity_SEVERITY_HIGH:
+		return "High"
+	case v1.Severity_SEVERITY_CRITICAL:
+		return "Critical"
+	default:
+		return "N/A"
+	}
+}
+
+func ConfidenceToText(confidence v1.Confidence) string {
+	switch confidence {
+	case v1.Confidence_CONFIDENCE_INFO:
+		return "Info"
+	case v1.Confidence_CONFIDENCE_LOW:
+		return "Low"
+	case v1.Confidence_CONFIDENCE_MEDIUM:
+		return "Medium"
+	case v1.Confidence_CONFIDENCE_HIGH:
+		return "High"
+	case v1.Confidence_CONFIDENCE_CRITICAL:
+		return "Critical"
+	default:
+		return "N/A"
+	}
+}
