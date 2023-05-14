54629a42eb49671fae5d32dddc9f5c7c15660fee
spyros <spyros@ocurity.com>
2023-05-14 17:16:54 +0100

    make templating auto apply default templates

--- pkg/templating/template_description.go
+++ pkg/templating/template_description.go
@@ -2,15 +2,17 @@ package templating
 
 import (
 	"bytes"
-	"fmt"
 	"text/template"
 
 	v1 "github.com/ocurity/dracon/api/proto/v1"
 )
 
+const defaultEnrichedFindingTemplate = "Dracon found '{{.RawIssue.Title}}' at '{{.RawIssue.Target}}', severity '{{.RawIssue.Severity}}', rule id: '{{.RawIssue.Type}}', CVSS '{{.RawIssue.Cvss}}' Confidence '{{.RawIssue.Confidence}}' Original Description: {{.RawIssue.Description}}, Cve {{.RawIssue.Cve}},\n{{ range $key,$element := .Annotations }}{{$key}}:{{$element}}\n{{end}}"
+const defaultRawFindingTemplate = "Dracon found '{{.Title}}' at '{{.Target}}', severity '{{.Severity}}', rule id: '{{.Type}}', CVSS '{{.Cvss}}' Confidence '{{.Confidence}}' Original Description: {{.Description}}, Cve {{.Cve}}"
+
 func TemplateStringRaw(inputTemplate string, issue *v1.Issue) (*string, error) {
 	if inputTemplate == "" {
-		return nil, fmt.Errorf("you need to specify a template")
+		inputTemplate = defaultRawFindingTemplate
 	}
 	tmpl, err := template.New("description").Parse(inputTemplate)
 	if err != nil {
@@ -28,7 +30,25 @@ func TemplateStringRaw(inputTemplate string, issue *v1.Issue) (*string, error) {
 }
 func TemplateStringEnriched(inputTemplate string, issue *v1.EnrichedIssue) (*string, error) {
 	if inputTemplate == "" {
-		return nil, fmt.Errorf("you need to specify a template")
+		inputTemplate = defaultEnrichedFindingTemplate
+	}
+	tmpl, err := template.New("description").Parse(inputTemplate)
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+
+	err = tmpl.Execute(buf, issue)
+	if err != nil {
+		return nil, err
+	}
+	res := buf.String()
+	return &res, nil
+
+}
+func TemplateStringJiraDocument(inputTemplate string, issue *v1.EnrichedIssue) (*string, error) {
+	if inputTemplate == "" {
+		inputTemplate = defaultEnrichedFindingTemplate
 	}
 	tmpl, err := template.New("description").Parse(inputTemplate)
 	if err != nil {
