8c3ff44c44c68e24475a8ea47a28a06bca86eeec
Spyros <northdpole@users.noreply.github.com>
2023-10-14 18:14:57 +0100

    Update components/consumers/aws-s3/main.go
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/consumers/aws-s3/main.go
+++ components/consumers/aws-s3/main.go
@@ -51,6 +51,7 @@ func main() {
 }
 
 func sendToS3(filename, bucket, region string, data []byte) {
+func sendToS3(filename, bucket, region string, data []byte) {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
 	},
