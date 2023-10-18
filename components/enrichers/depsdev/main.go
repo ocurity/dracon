0375d7668ec273cf8fa639e209833f75b5f77c0c
foobar <spyros@ocurity.com>
2023-10-18 22:31:46 +0100

    lint

--- components/enrichers/depsdev/main.go
+++ components/enrichers/depsdev/main.go
@@ -30,6 +30,7 @@ var (
 	annotation         string
 )
 
+// Check is a deps.dev ScoreCardV2 check
 type Check struct {
 	Name          string `json:"name,omitempty"`
 	Documentation struct {
@@ -40,6 +41,8 @@ type Check struct {
 	Reason  string        `json:"reason,omitempty"`
 	Details []interface{} `json:"details,omitempty"`
 }
+
+// ScorecardV2 is a deps.dev ScoreCardV2 result
 type ScorecardV2 struct {
 	Date string `json:"date,omitempty"`
 	Repo struct {
@@ -54,6 +57,8 @@ type ScorecardV2 struct {
 	Metadata []interface{} `json:"metadata,omitempty"`
 	Score    float64       `json:"score,omitempty"`
 }
+
+// Project is a deps.dev project
 type Project struct {
 	Type        string      `json:"type,omitempty"`
 	Name        string      `json:"name,omitempty"`
@@ -67,6 +72,8 @@ type Project struct {
 	Link        string      `json:"link,omitempty"`
 	ScorecardV2 ScorecardV2 `json:"scorecardV2,omitempty"`
 }
+
+// Project is a deps.dev version
 type Version struct {
 	Version                string        `json:"version,omitempty"`
 	SymbolicVersions       []interface{} `json:"symbolicVersions,omitempty"`
