e641f41f41b28027f722d81e9741568636feac9d
Spyros <northdpole@users.noreply.github.com>
2023-12-28 00:13:23 +0000

    Update pkg/context/context_test.go
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkg/context/context_test.go
+++ pkg/context/context_test.go
@@ -83,6 +83,7 @@ func TestExtractCodeLine(t *testing.T) {
 	assert.Equal(t, strings.Join(strings.Split(code, "\n")[17-DefaultLineRange:17+DefaultLineRange], "\n"), codeRange)
 }
 
+
 func TestExtractCodeInvalidTarget(t *testing.T) {
 	// target is ip, url or file that does not exist
 
