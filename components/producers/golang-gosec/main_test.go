ec24640626619eabb19f8c66862501371acd7c73
foobar <spyros@ocurity.com>
2023-12-28 00:05:50 +0000

    add a small library that allows for adding code context to issues

--- components/producers/golang-gosec/main_test.go
+++ components/producers/golang-gosec/main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"os"
 	"testing"
 
 	v1 "github.com/ocurity/dracon/api/proto/v1"
@@ -9,7 +11,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const exampleOutput = `
+var code = `func GetProducts(ctx context.Context, db *sql.DB, category string) ([]Product, error) {
+	rows, err := db.QueryContext(ctx, "SELECT * FROM product WHERE category='"+category+"'")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var product Product
+		if err := rows.Scan(&product.Id, &product.Name, &product.Category, &product.Price); err != nil {
+			return nil, err
+		}`
+
+func TestParseIssues(t *testing.T) {
+	file, err := os.CreateTemp("", "dracon_context_test")
+	if err != nil {
+		t.Errorf("could not setup tests for context pkg, could not create temporary files")
+	}
+	defer os.Remove(file.Name())
+	if err := os.WriteFile(file.Name(), []byte(code), os.ModeAppend); err != nil {
+		t.Errorf("could not setup tests for context pk, could not write temporary file")
+	}
+
+	exampleOutput := fmt.Sprintf(`
 {
 	"Issues": [
 		{
@@ -17,9 +42,9 @@ const exampleOutput = `
 			"confidence": "HIGH",
 			"rule_id": "G304",
 			"details": "Potential file inclusion via variable",
-			"file": "/tmp/source/foo.go",
+			"file": "%s",
 			"code": "ioutil.ReadFile(path)",
-			"line": "33",
+			"line": "2",
 			"column": "44"
 		}
 	],
@@ -29,23 +54,22 @@ const exampleOutput = `
 		"nosec": 0,
 		"found": 1
 	}
-}`
-
-func TestParseIssues(t *testing.T) {
+}`, file.Name())
 	var results GoSecOut
-	err := json.Unmarshal([]byte(exampleOutput), &results)
+	err = json.Unmarshal([]byte(exampleOutput), &results)
 	assert.Nil(t, err)
 
-	issues := parseIssues(&results)
-
+	issues, err := parseIssues(&results)
+	assert.Nil(t, err)
 	expectedIssue := &v1.Issue{
-		Target:      "/tmp/source/foo.go:33",
-		Type:        "G304",
-		Title:       "Potential file inclusion via variable",
-		Severity:    v1.Severity_SEVERITY_MEDIUM,
-		Cvss:        0.0,
-		Confidence:  v1.Confidence_CONFIDENCE_HIGH,
-		Description: "ioutil.ReadFile(path)",
+		Target:         fmt.Sprintf("%s:2", file.Name()),
+		Type:           "G304",
+		Title:          "Potential file inclusion via variable",
+		Severity:       v1.Severity_SEVERITY_MEDIUM,
+		Cvss:           0.0,
+		Confidence:     v1.Confidence_CONFIDENCE_HIGH,
+		Description:    "ioutil.ReadFile(path)",
+		ContextSegment: &code,
 	}
 
 	assert.Equal(t, []*v1.Issue{expectedIssue}, issues)
---
+++ pkg/context/context.go
@@ -0,0 +1,80 @@
+// Package context offers a set of methods which permit components to
+//  add information about the context of each vulnerability
+//  this information is highly depended on the actual vulnerability and the component.
+//  For example for SAST components, context can be a call graph or
+//  a few lines of code before and after the line that triggered the finding.
+//  For DAST components it can be a serialised request/response.
+
+package context
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	v1 "github.com/ocurity/dracon/api/proto/v1"
+)
+
+const DefaultLineRange = 10
+
+// ExtractCode takes a path and line (or line range) for a vulnerable code segment and
+// returns the DefaultLineRange lines above and the DefaultLineRange lines below the vulnerability.
+// It does not take into account end of function.
+func ExtractCode(finding *v1.Issue) (string, error) {
+	path := ""
+	lineRange := ""
+	lineFrom := 0
+	lineTo := 0
+
+	split := strings.Split(finding.Target, ":")
+	if len(split) < 2 {
+		path = finding.Target
+		lineRange = "0"
+	} else {
+		path = split[0]
+		lineRange = split[1]
+	}
+	if strings.Contains(lineRange, "-") {
+		lines := strings.Split(lineRange, "-")
+		lf, err := strconv.Atoi(lines[0])
+		if err != nil {
+			return "", err
+		}
+		lineFrom = lf
+		lt, err := strconv.Atoi(lines[1])
+		if err != nil {
+			return "", err
+		}
+		lineTo = lt
+
+	} else {
+		lf, err := strconv.Atoi(lineRange)
+		if err != nil {
+			return "", err
+		}
+		lineFrom = lf
+		lineTo = lf
+	}
+	if lineFrom < DefaultLineRange {
+		lineFrom = 0
+	} else {
+		lineFrom = lineFrom - DefaultLineRange
+	}
+	lineTo = lineTo + DefaultLineRange
+	handle, err := os.Open(path)
+	if err != nil {
+		return "", fmt.Errorf("context pkg could not open file in path %s, err:%v", path, err)
+	}
+	sc := bufio.NewScanner(handle)
+	pos := 0
+	lines := []string{}
+	for sc.Scan() {
+		if lineFrom <= pos && pos < lineTo {
+			lines = append(lines, sc.Text())
+		}
+		pos++
+	}
+	return strings.Join(lines, "\n"), nil
+}
