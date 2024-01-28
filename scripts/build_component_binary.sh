c171c51361f5cb5e750e8dffa246957545dc1ca0
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2024-01-28 11:48:18 +0000

    Add make targets for building component kustomizations (Part of #62)
    This commit removes all BUILD files from the components and replaces
    them with two scripts in the scripts folder and make targets that
    automate building all component containers. The Go binaries are first
    built using a make target with the `/bin` suffix and the path of the
    directory of the component. The binaries are then packaged into
    containers using `docker`. Each container is tagged with a tag that is
    produced based on the git tag of the commit. If the current commit is
    not tagged, the container tag will be the commit SHA + how many commits
    ahead of the latest tagged commit + the latest tag in the branch's
    history.
    For some components, extra functionality is needed because we use a
    different name for the component or some extra functionality. For these
    cases, we allow a special Makefile to be placed in the directory and we
    expect targets with specific names to be present. If they are, the top
    level Makefile will invoke them and allow them to build the component in
    whatever way they want.
    
    Signed-off-by: Pavlos Tzianos <pavlos.tzianos@gmail.com>
    Signed-off-by: Pavlos Tzianos <pavlos@ocurity.com>

---
+++ scripts/build_component_binary.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+source ./scripts/util.sh
+
+if [ "$#" -eq 0 ]
+then
+    util::error "No directory provided to build"
+    exit 1
+fi
+
+executable=$(basename $(dirname "${1})"))
+
+echo "${1}" | grep -Eq ^components/producers/.*$ && executable="${executable}-parser" || true
+echo "${1}" | grep -Eq ^components/enrichers/.*$ && executable="${executable}" || true
+echo "${1}" | grep -Eq ^components/consumers/.*$ && executable="${executable}" || true
+
+executable_src_path=$(dirname "${1}")
+executable_path=$(dirname $(dirname "${1}"))/"${executable}"
+
+echo "building bin/${executable_path}/${executable}" > /dev/stderr
+
+go build -o "bin/${executable_src_path}/${executable}" "./${executable_src_path}/main.go"
---
+++ scripts/build_component_container.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e;
+
+source ./scripts/util.sh
+
+if [ "$#" -eq 0 ]
+then
+    util::error "No directory provided to build"
+    exit 1
+fi
+
+executable=$(basename $(dirname "${1}"))
+
+echo "${1}" | grep -Eq ^components/producers/.*$ && executable="${executable}-parser" || true
+echo "${1}" | grep -Eq ^components/enrichers/.*$ && executable="${executable}" || true
+echo "${1}" | grep -Eq ^components/consumers/.*$ && executable="${executable}" || true
+
+executable_src_path=$(dirname "${1}")
+executable_path=$(dirname "${1}")/"${executable}"
+
+if make -C "${executable_src_path}" --no-print-directory --dry-run container >/dev/null 2>&1
+then
+    make -C "${executable_src_path}" --no-print-directory --quiet container CONTAINER_REPO="${CONTAINER_REPO}" DRACON_VERSION="${DRACON_VERSION}"
+else
+    dockerfile_template="
+        FROM scratch                                    \n
+        COPY ${executable_path} /app/${executable_path} \n
+        ENTRYPOINT ["/app/${executable_path}"]          \n
+    "
+    dockerfile_path=$(mktemp)
+    printf "${dockerfile_template}" > "${dockerfile_path}"
+    docker build -t "${CONTAINER_REPO}/${executable_src_path}:${DRACON_VERSION}" -f "${dockerfile_path}" ./bin
+fi
+
+if make -C "${executable_src_path}" --no-print-directory --dry-run extras >/dev/null 2>&1
+then
+    make -C "${executable_src_path}" --no-print-directory --quiet package-extras DOCKER_REPO="${DOCKER_REPO}" DRACON_VERSION="${DRACON_VERSION}"
+fi
