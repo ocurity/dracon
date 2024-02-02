f3dcf2e0fe6354d4399230da0c56aacc1f52765a
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2024-02-02 16:07:54 +0000

    Add Bash util script
    Signed-off-by: Pavlos Tzianos <pavlos.tzianos@gmail.com>

--- scripts/util.sh
+++ scripts/util.sh
@@ -2,7 +2,7 @@
 # This script contains general helper functions for bash scripting.
 set -Eeuo pipefail
 
-source "./third_party/sh/ansi"
+source ./third_party/sh/ansi.sh
 
 util::debug() {
     set -x; "$@"; set +x;
