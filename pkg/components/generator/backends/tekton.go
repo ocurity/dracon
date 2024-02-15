084ac54efac06ec2d3116979cc5f2917e649456c
foobar <spyros@ocurity.com>
2024-02-15 18:45:52 +0000

    remove renaming of task parameters:

--- pkg/components/generator/backends/tekton.go
+++ pkg/components/generator/backends/tekton.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"slices"
-	"strings"
 
 	"github.com/ocurity/dracon/pkg/components"
 	tektonV1Beta1 "github.com/ocurity/dracon/pkg/types/tekton.dev/v1beta1"
@@ -19,39 +18,10 @@ type tektonBackend struct {
 	suffix   string
 }
 
-func renameParameterRef(oldVal string, newParameterNames map[string]string) string {
-	if !strings.HasPrefix(oldVal, "$(params.") {
-		return oldVal
-	}
-	oldParamRef := strings.Split(oldVal, "$(params.")[1]
-	// remove last parentheses
-	oldParamRef = oldParamRef[:len(oldParamRef)-1]
-	return fmt.Sprintf("$(params.%s)", newParameterNames[oldParamRef])
-}
-
 // fixTaskPrefixSuffix adds a prefix and a suffix to the name of the task and all the task
 // parameters. Having task parameters prefixed with the same name as the task itself, helps
 // users figure out more easily which parameters configure what.
 func fixTaskPrefixSuffix(task tektonV1Beta1.Task, prefix, suffix string) {
-	// keep track of renamings so that we can also fix the environment variables
-	// referencing the parameters
-	newParameterNames := map[string]string{}
-
-	for _, param := range task.Spec.Parameters {
-		oldParamName := param.Name
-		paramNameChunks := strings.Split(param.Name, task.Metadata.Name)
-		param.Name = prefix + task.Metadata.Name + suffix + paramNameChunks[1]
-		newParameterNames[oldParamName] = param.Name
-	}
-
-	// fix references to parameters in step env vars and images
-	for _, step := range task.Spec.Steps {
-		for _, env := range step.Env {
-			env.Value = renameParameterRef(env.Value, newParameterNames)
-		}
-		step.Image = renameParameterRef(step.Image, newParameterNames)
-	}
-
 	task.Metadata.Name = prefix + task.Metadata.Name + suffix
 }
 
