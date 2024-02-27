9e354be066475531c92d18ab3ab7c7d731a5a621
foobar <spyros@ocurity.com>
2024-02-27 13:56:55 +0000

    dependency-track parser

---
+++ components/producers/dependency-track/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	v1 "github.com/ocurity/dracon/api/proto/v1"
+
+	"github.com/ocurity/dracon/components/producers"
+)
+
+func main() {
+	if err := producers.ParseFlags(); err != nil {
+		log.Fatal(err)
+	}
+
+	inFile, err := producers.ReadInFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results DependencyTrackOut
+	if err := producers.ParseJSON(inFile, &results); err != nil {
+		log.Fatal(err)
+	}
+
+	issues, err := parseIssues(&results)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := producers.WriteDraconOut(
+		"gosec",
+		issues,
+	); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func parseIssues(out *DependencyTrackOut) ([]*v1.Issue, error) {
+	issues := []*v1.Issue{}
+	for _, element := range *out {
+		iss := v1.Issue{}
+		target := element.Component.Purl
+		iss.Target = target
+		cwe := fmt.Sprintf("%d", element.Vulnerability.CweID)
+		iss.Type = element.Vulnerability.VulnID
+		iss.Title = element.Vulnerability.Title
+		iss.Cvss = element.Vulnerability.CvssV3BaseScore
+		iss.Severity = v1.Severity(v1.Severity_value[fmt.Sprintf("SEVERITY_%s", element.Vulnerability.Severity)])
+		iss.Cwe = &cwe
+		if len(element.Vulnerability.Aliases) > 0 {
+			iss.Cve = element.Vulnerability.Aliases[0].CveID
+		}
+		iss.Description = fmt.Sprintf("%s\n%s", element.Vulnerability.Description, element.Vulnerability.Recommendation)
+		issues = append(issues, &iss)
+	}
+
+	return issues, nil
+}
+
+type Aliases []struct {
+	CveID  string `json:"cveId"`
+	SnykID string `json:"snykId"`
+}
+type Component struct {
+	UUID          string `json:"uuid"`
+	Name          string `json:"name"`
+	Group         string `json:"group"`
+	Version       string `json:"version"`
+	Purl          string `json:"purl"`
+	Project       string `json:"project"`
+	LatestVersion string `json:"latestVersion"`
+}
+type Vulnerability struct {
+	UUID            string  `json:"uuid"`
+	Source          string  `json:"source"`
+	VulnID          string  `json:"vulnId"`
+	Title           string  `json:"title"`
+	CvssV3BaseScore float64 `json:"cvssV3BaseScore"`
+	Severity        string  `json:"severity"`
+	SeverityRank    int     `json:"severityRank"`
+	CweID           int     `json:"cweId"`
+	CweName         string  `json:"cweName"`
+	Cwes            []struct {
+		CweID int    `json:"cweId"`
+		Name  string `json:"name"`
+	} `json:"cwes"`
+	Aliases        Aliases `json:"aliases"`
+	Description    string  `json:"description"`
+	Recommendation string  `json:"recommendation"`
+}
+type DependencyTrackOut []struct {
+	Component     Component     `json:"component"`
+	Vulnerability Vulnerability `json:"vulnerability"`
+
+	Analysis struct {
+		IsSuppressed bool `json:"isSuppressed"`
+	} `json:"analysis"`
+	Attribution struct {
+		AnalyzerIdentity string `json:"analyzerIdentity"`
+		AttributedOn     int64  `json:"attributedOn"`
+	} `json:"attribution"`
+	Matrix string `json:"matrix"`
+}
