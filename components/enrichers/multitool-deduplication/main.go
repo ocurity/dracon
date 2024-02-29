fa34c161798eecb8595557ad16c8c003f7929d3f
foobar <spyros@ocurity.com>
2024-02-29 20:40:58 +0000

    init multi tool deduplication

---
+++ components/enrichers/multitool-deduplication/main.go
@@ -0,0 +1,242 @@
+// package main of the multitool deduplication enricher finds and tags issues found by multiple tools in this scan
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	v1 "github.com/ocurity/dracon/api/proto/v1"
+
+	"github.com/ocurity/dracon/pkg/putil"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// for every tool response
+//  for every issue
+// 		if issue target is purl
+// 			func handle purls(sca findings)
+// 			if the issue has a CVE attached
+// 				if cveMap[cve] != nil
+// 					mark dup, attach which other tool found it
+// 				else
+//	 				map the cve to the issue
+// 			else if the issue does not have a CVE attached
+//  TODO match if the tool is SCA, and match output title and remote references to some CRE, then try to match to CREs of other tools
+// 		if issue target is code (file/system/path:line or file/system/path:line-range)
+// 			func handle sast
+// 				if the issue has a CWE
+// 					if cweMap[cwe] != nil
+// 						if cweMap[cwe] target and line (range)  includes the line and targer of current issue
+// 							mark dup, attach which other tool found it
+// 						else
+// 							append to cweMap
+// 					else
+// 						cweMap[cwe] = []v1.Issue{issue}
+//				else if the issue does not have a CWE
+// TODO match if the tool is SAST, match title and description (and content of remote references) to CRE, try to match to CREs of other tools
+
+const AnnotationStr = "Found By Other Tools"
+
+var (
+	connStr   string
+	readPath  string
+	writePath string
+	cve       map[string][]tooledIssue = map[string][]tooledIssue{}
+	// cwe                 map[int32][]tooledIssue  = map[string][]tooledIssue{}
+	// cre                 map[string][]tooledIssue = map[string][]tooledIssue{}
+	filesystemTargetReg = `(?P<path>^(.+)\/([^\/]+)/?)(?P<range>(?P<lineFrom>:\d+)(?P<lineTo> ?- ? \d+)?)?$`
+)
+
+type tooledIssue struct {
+	issue         *v1.Issue
+	producingTool string
+}
+
+// type lineRange struct {
+// 	filePath  string
+// 	lineStart int
+// 	lineEnd   int
+// }
+
+// func convertTargetToLineRange(target string) lineRange {
+// 	result := lineRange{}
+// 	compiledRegexp := regexp.MustCompile(filesystemTargetReg)
+// 	matches := compiledRegexp.FindStringSubmatch(target)
+// 	result.filePath = matches[compiledRegexp.SubexpIndex("path")]
+// 	lineStart := strings.TrimSpace(strings.ReplaceAll(matches[compiledRegexp.SubexpIndex("lineFrom")], ":", ""))
+// 	if lineStart != "" {
+// 		ls, err := strconv.Atoi(lineStart)
+// 		if err != nil {
+// 			log.Println("Could not recognise line start for issue target", target)
+// 		}
+// 		result.lineStart = ls
+// 	} else {
+// 		fmt.Printf("%#v\n", matches)
+// 	}
+// 	lineEnd := strings.TrimSpace(strings.ReplaceAll(matches[compiledRegexp.SubexpIndex("lineTo")], "-", ""))
+// 	if lineEnd != "" {
+// 		le, err := strconv.Atoi(lineEnd)
+// 		if err != nil {
+// 			log.Println("Could not recognise line end for issue target", target)
+// 		}
+// 		result.lineEnd = le
+// 	} else {
+// 		fmt.Printf("%#v\n", matches)
+// 	}
+// 	return result
+// }
+
+// func handleFilesystem(issue *v1.Issue, issueTool string) string {
+// 	target := convertTargetToLineRange(issue.Target)
+// 	if len(issue.Cwe) > 0 {
+// 		for _, cweNum := range issue.Cwe {
+// 			cweIssues, ok := cwe[cweNum]
+// 			if ok {
+// 				for _, cweIssue := range cweIssues {
+// 					matchedTarget := convertTargetToLineRange(cweIssue.issue.Target)
+// 					if matchedTarget.filePath == target.filePath { // if we are on the same file
+// 						if (matchedTarget.lineStart <= target.lineStart && // and line range for existing vuln is larger than line range for new vuln
+// 							matchedTarget.lineEnd >= target.lineEnd) ||
+// 							(matchedTarget.lineStart >= target.lineStart && // or if the line range for the new vuln contains the line range for the existing vuln
+// 								matchedTarget.lineEnd <= target.lineEnd) {
+// 							return cweIssue.producingTool
+// 						}
+// 					}
+// 				}
+// 			} else {
+// 				cwe[cweNum] = []tooledIssue{{issue: issue, producingTool: issueTool}}
+// 			}
+// 		}
+// 	}
+// 	// @TODO(spyros) match if the tool is SAST, match title and description (and content of remote references) to CRE, try to match to CREs of other tools
+// 	return ""
+// }
+
+func handlePurl(issue *v1.Issue, issueTool string) string {
+	if issue.Cve != "" {
+		cveIssues, ok := cve[issue.Cve]
+		if ok {
+			return cveIssues[0].producingTool
+		}
+		cve[issue.Cve] = []tooledIssue{{
+			issue:         issue,
+			producingTool: issueTool,
+		}}
+	}
+	//@TODO(spyros): match if the tool is SCA, and match output title and remote references to some CRE, then try to match to CREs of other tools
+	return ""
+}
+
+func enrichIssue(issue *v1.Issue, foundTool string) v1.EnrichedIssue {
+	matchFilesystem := regexp.MustCompile(filesystemTargetReg)
+	if strings.HasPrefix(issue.Target, "pkg") {
+		if tool := handlePurl(issue, foundTool); tool != "" {
+			if foundTool != tool {
+				return v1.EnrichedIssue{
+					RawIssue:    issue,
+					Annotations: map[string]string{AnnotationStr: "true"},
+				}
+			} else {
+				log.Println("Duplicate CVE found for tool", tool, "and CVE", issue.Cve)
+				return v1.EnrichedIssue{
+					RawIssue: issue,
+				}
+			}
+		}
+	} else if matchFilesystem.Match([]byte(issue.Target)) {
+		return v1.EnrichedIssue{
+			RawIssue: issue,
+		}
+		// SAST not supported yet!!!
+		// 	if tool := handleFilesystem(issue, foundTool); tool != "" {
+		// 		if foundTool != tool {
+		// 			return v1.EnrichedIssue{
+		// 				RawIssue:    issue,
+		// 				Annotations: map[string]string{AnnotationStr: "true"},
+		// 			}
+		// 		} else {
+		// 			log.Println("Duplicate CWE found for tool", tool, "and CWEs", issue.Cwe)
+		// 			return v1.EnrichedIssue{
+		// 				RawIssue: issue,
+		// 			}
+		// 		}
+		// 	}
+	}
+	log.Println("Target", issue.Target, "is not supported, at this time this enricher only supports filesystem targets or purls")
+	return v1.EnrichedIssue{
+		RawIssue: issue,
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "enricher",
+	Short: "enricher",
+	Long:  "tool to enrich issues against a database",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		readPath = viper.GetString("read_path")
+		res, err := putil.LoadTaggedToolResponse(readPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Loaded %d tagged tool responses\n", len(res))
+		writePath = viper.GetString("write_path")
+		for _, r := range res {
+			enrichedIssues := []*v1.EnrichedIssue{}
+			for _, i := range r.GetIssues() {
+				eI := enrichIssue(i, r.ToolName)
+				enrichedIssues = append(enrichedIssues, &eI)
+				log.Printf("enriched issue '%s'", eI.GetRawIssue().GetUuid())
+			}
+			if len(enrichedIssues) > 0 {
+				if err := putil.WriteEnrichedResults(r, enrichedIssues,
+					filepath.Join(writePath, fmt.Sprintf("%s.enriched.pb", r.GetToolName())),
+				); err != nil {
+					return err
+				}
+			}
+			if len(r.GetIssues()) > 0 {
+				scanStartTime := r.GetScanInfo().GetScanStartTime().AsTime()
+				if err := putil.WriteResults(
+					r.GetToolName(),
+					r.GetIssues(),
+					filepath.Join(writePath, fmt.Sprintf("%s.raw.pb", r.GetToolName())),
+					r.GetScanInfo().GetScanUuid(),
+					scanStartTime.Format(time.RFC3339),
+				); err != nil {
+					log.Fatalf("could not write results: %s", err)
+				}
+			}
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.Flags().StringVar(&connStr, "db_connection", "", "the database connection DSN")
+	rootCmd.Flags().StringVar(&readPath, "read_path", "", "the path to read LaunchToolResponses from")
+	rootCmd.Flags().StringVar(&writePath, "write_path", "", "the path to write enriched results to")
+	if err := viper.BindPFlag("db_connection", rootCmd.Flags().Lookup("db_connection")); err != nil {
+		log.Fatalf("could not bind db_connection flag: %s", err)
+	}
+	if err := viper.BindPFlag("read_path", rootCmd.Flags().Lookup("read_path")); err != nil {
+		log.Fatalf("could not bind read_path flag: %s", err)
+	}
+	if err := viper.BindPFlag("write_path", rootCmd.Flags().Lookup("write_path")); err != nil {
+		log.Fatalf("could not bind write_path flag: %s", err)
+	}
+	viper.SetEnvPrefix("enricher")
+	viper.AutomaticEnv()
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ components/enrichers/multitool-deduplication/main_test.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	v1 "github.com/ocurity/dracon/api/proto/v1"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+const (
+	license = "Foo License v0"
+)
+
+func genScanCVE(toolName string, targets []string, cves []string) *v1.LaunchToolResponse { // gen SCA scan
+	id := uuid.New()
+	startTime := timestamppb.Now()
+	scan := v1.LaunchToolResponse{
+		ScanInfo: &v1.ScanInfo{ScanUuid: id.String(), ScanStartTime: startTime},
+		ToolName: toolName,
+		Issues:   []*v1.Issue{},
+	}
+	for i, t := range targets {
+		scan.Issues = append(scan.Issues, &v1.Issue{
+			Target:   t,
+			Type:     fmt.Sprintf("someID %d", i),
+			Title:    fmt.Sprintf("someTitle %d", i),
+			Severity: v1.Severity_SEVERITY_UNSPECIFIED,
+			Cvss:     float64(i),
+			Cve:      cves[i%len(cves)],
+		})
+	}
+	return &scan
+}
+
+func genScanCWE(toolName string, targets []string, cwes []int32) *v1.LaunchToolResponse { // gen SAST Scan
+	id := uuid.New()
+	startTime := timestamppb.Now()
+	scan := v1.LaunchToolResponse{
+		ScanInfo: &v1.ScanInfo{ScanUuid: id.String(), ScanStartTime: startTime},
+		ToolName: toolName,
+		Issues:   []*v1.Issue{},
+	}
+	for i, t := range targets {
+		scan.Issues = append(scan.Issues, &v1.Issue{
+			Target:   t,
+			Type:     fmt.Sprintf("someID %d", i),
+			Title:    fmt.Sprintf("someTitle %d", i),
+			Severity: v1.Severity_SEVERITY_UNSPECIFIED,
+			Cvss:     float64(i),
+			Cwe:      cwes,
+		})
+	}
+	return &scan
+}
+func TestHandlePurl(t *testing.T) {
+	cve := []string{}
+	targets := []string{}
+	for i := 0; i <= 10; i++ {
+		cve = append(cve, fmt.Sprintf("CVE-2009-%d", i+500))
+		targets = append(targets, fmt.Sprintf("/foo/bar/target%d:%d", i%3, i%3))
+	}
+	scan1 := genScanCVE("tool1", targets, cve)
+	scan2 := genScanCVE("tool2", targets, cve)
+	for _, issue := range scan1.GetIssues() {
+		assert.Equal(t, "", handlePurl(issue, scan1.ToolName))
+	}
+	for _, issue := range scan1.GetIssues() {
+		assert.Equal(t, "tool1", handlePurl(issue, scan2.ToolName))
+	}
+}
+
+// func TestConvertTargetToLineRange(t *testing.T) {
+// 	type dat struct {
+// 		input  string
+// 		output lineRange
+// 	}
+// 	data := []dat{
+// 		{
+// 			input:  "/positive/max/example.blah:234-235",
+// 			output: lineRange{filePath: "/positive/max/example.blah", lineStart: 234, lineEnd: 235},
+// 		},
+// 		// {
+// 		// 	input:  "no/leading/slash.foo:123-124",
+// 		// 	output: lineRange{filePath: "no/leading/slash.foo", lineStart: 123, lineEnd: 124},
+// 		// },
+// 		// {input: "single/line/in/range.go:595", output: lineRange{filePath: "single/line/in/range.go", lineStart: 595, lineEnd: 0}},
+// 		// {input: "with/spaces.go : 595", output: lineRange{filePath: "with/spaces", lineStart: 595, lineEnd: 0}},
+// 		// {input: "with/spaces/range.go : 595 - 596", output: lineRange{filePath: "with/spaces/range.go", lineStart: 595, lineEnd: 596}},
+// 	}
+// 	for _, d := range data {
+// 		assert.Equal(t, d.output, convertTargetToLineRange(d.input))
+// 	}
+// }
+
+// func TestHandleFilesystem(t *testing.T) {
+// 	cwe := []int32{}
+// 	targets := []string{}
+// 	for i := 0; i <= 10; i++ {
+// 		cwe = append(cwe, int32(i+500))
+// 		targets = append(targets, fmt.Sprintf("/foo/bar/target%d:%d", i%3, i%3))
+// 	}
+// 	scan1 := genScanCWE("tool1", targets, cwe)
+// 	scan2 := genScanCWE("tool2", targets, cwe)
+// 	for _, issue := range scan1.GetIssues() {
+// 		assert.Equal(t, "", handleFilesystem(issue, scan1.ToolName))
+// 	}
+// 	for _, issue := range scan1.GetIssues() {
+// 		assert.Equal(t, "tool2", handleFilesystem(issue, scan2.ToolName))
+// 	}
+// }
