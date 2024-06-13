907c64936f44e49b14e82d2a2b8cd29130829db2
dlicheva <diana@ocurity.com>
2024-06-13 19:06:11 +0100

    componentType enum with go-enum

--- pkg/components/metadata.go
+++ pkg/components/metadata.go
@@ -1,8 +1,7 @@
 package components
 
 import (
-	"bytes"
-	"fmt"
+	"slices"
 
 	"github.com/go-errors/errors"
 )
@@ -12,87 +11,8 @@ import (
 const LabelKey string = "v1.dracon.ocurity.com/component"
 
 // ComponentType represents all the types of components that Dracon supports
-type ComponentType int
-
-const (
-	// Unknown is the default component type value
-	Unknown ComponentType = iota
-	// Base represents the base component of a pipeline
-	Base
-	// Source represents the source component of a pipeline
-	Source
-	// Producer represents the producer component of a pipeline
-	Producer
-	// ProducerAggregator represents the producer aggregator component of a
-	// pipeline
-	ProducerAggregator
-	// Enricher represents the enricher component of a pipeline
-	Enricher
-	// EnricherAggregator represents the enricher aggregator component of a
-	// pipeline
-	EnricherAggregator
-	// Consumer represents the consumer component of a pipeline
-	Consumer
-)
-
-// String converts the ComponentType enum value to a string
-func (ct ComponentType) String() string {
-	switch ct {
-	case Unknown:
-		return "unknown"
-	case Base:
-		return "base"
-	case Source:
-		return "source"
-	case Producer:
-		return "producer"
-	case ProducerAggregator:
-		return "producer-aggregator"
-	case Enricher:
-		return "enricher"
-	case EnricherAggregator:
-		return "enricher-aggregator"
-	case Consumer:
-		return "consumer"
-	default:
-		panic(fmt.Sprintf("unknown component type: %d", ct))
-	}
-}
-
-// ToComponentType converts a string into a ComponentType enum value or
-// returns an error
-func ToComponentType(cts string) (ComponentType, error) {
-	switch cts {
-	case "unknown":
-		return Unknown, nil
-	case "base":
-		return Base, nil
-	case "source":
-		return Source, nil
-	case "producer":
-		return Producer, nil
-	case "producer-aggregator":
-		return ProducerAggregator, nil
-	case "enricher":
-		return Enricher, nil
-	case "enricher-aggregator":
-		return EnricherAggregator, nil
-	case "consumer":
-		return Consumer, nil
-	default:
-		return Base, fmt.Errorf("%s: unknown component type", cts)
-	}
-}
-
-// MustGetComponentType converts a string into a ComponentType enum value or
-// panics
-func MustGetComponentType(cts string) ComponentType {
-	ct, err := ToComponentType(cts)
-	if err != nil {
-		panic(err)
-	}
-	return ct
-}
+// ENUM(unknown, base, source, producer, producer-aggregator, enricher, enricher-aggregator, consumer)
+type ComponentType string
 
 // LabelValueOneOf checks if the labels map has the expected key set and if
 // that key has any one of the expected values
@@ -102,7 +22,7 @@ func LabelValueOneOf(labels map[string]string, componentTypes ...ComponentType) 
 		return false, errors.Errorf("no %s key in labels", LabelKey)
 	}
 
-	labelCt, err := ToComponentType(labelValue)
+	labelCt, err := ParseComponentType(labelValue)
 	if err != nil {
 		return false, err
 	}
@@ -116,51 +36,29 @@ func LabelValueOneOf(labels map[string]string, componentTypes ...ComponentType) 
 	return false, nil
 }
 
-// Equal checks if the other is a ComponentType or some type that can be parsed
-// and then checks for equality.
-func (ct ComponentType) Equal(other any) (bool, error) {
-	switch o := other.(type) {
-	case string:
-		parsed, err := ToComponentType(o)
-		if err != nil {
-			return false, err
-		}
-		return parsed == ct, nil
-	case ComponentType:
-		return ct == o, nil
-	case fmt.Stringer:
-		return ct.Equal(o.String())
-	default:
-		return false, errors.Errorf("unknown component type")
+// GetPrevious returns previous component type
+// e.g. If we want the previous component type of Producer, we get Source
+func GetPrevious(componentType ComponentType) ComponentType {
+	allComponentTypes := ComponentTypeValues()
+	currentTypeIndex := slices.Index(allComponentTypes, componentType)
+
+	previousComponentType := Base
+	if currentTypeIndex >= 1 {
+		previousComponentType = allComponentTypes[currentTypeIndex-1]
 	}
+
+	return previousComponentType
 }
 
-// MarshalJSON marshals a `ComponentType` into JSON bytes
-func (ct ComponentType) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + ct.String() + `"`), nil
+// ADifferenceFromB returns the difference in position between ComponentType A and ComponentType B
+// e.g. Enricher is 2 steps further along the pipeline than Producer
+func ADifferenceFromB(componentTypeA ComponentType, componentTypeB ComponentType) int {
+	allComponentTypes := ComponentTypeValues()
+	return slices.Index(allComponentTypes, componentTypeA) - slices.Index(allComponentTypes, componentTypeB)
 }
 
-// MarshalText marshals the `ComponentType` into text bytes
-func (ct ComponentType) MarshalText() ([]byte, error) {
-	return []byte(`"` + ct.String() + `"`), nil
-}
-
-// UnmarshalJSON unmarshalls bytes into a `ComponentType`
-func (ct *ComponentType) UnmarshalJSON(b []byte) error {
-	b = bytes.Trim(bytes.Trim(b, `"`), ` `)
-	parsedComponent, err := ToComponentType(string(b))
-	if err == nil {
-		*ct = parsedComponent
-	}
-	return err
-}
-
-// UnmarshalText unmarshalls bytes into a `ComponentType`
-func (ct *ComponentType) UnmarshalText(text []byte) error {
-	text = bytes.Trim(bytes.Trim(text, `"`), ` `)
-	parsedComponent, err := ToComponentType(string(text))
-	if err == nil {
-		*ct = parsedComponent
-	}
-	return err
+// AGreaterThanB returns whether ComponentType A is further along the pipeline than ComponentType B
+// e.g. Enricher is further along the pipeline than Producer
+func AGreaterThanB(componentTypeA ComponentType, componentTypeB ComponentType) bool {
+	return ADifferenceFromB(componentTypeA, componentTypeB) > 0
 }
--- pkg/pipelines/tektonv1beta1.go
+++ pkg/pipelines/tektonv1beta1.go
@@ -116,7 +116,7 @@ func (k k8sOrchestrator) Prepare(ctx context.Context, pipelineComponents []compo
 				return errors.Errorf("%s: task does not have a component type label", component.Name)
 			}
 
-			componentType, err := components.ToComponentType(componentTypeLabel)
+			componentType, err := components.ParseComponentType(componentTypeLabel)
 			if err != nil {
 				return errors.Errorf("%s: task has wrong component type: %w", component.Name, err)
 			}
@@ -142,9 +142,9 @@ func (k k8sOrchestrator) Deploy(ctx context.Context, basePipeline *tektonv1beta1
 
 	// Sort tasks based on their component type
 	slices.SortFunc(taskList, func(a *tektonv1beta1api.Task, b *tektonv1beta1api.Task) int {
-		componentTypeA := components.MustGetComponentType(a.Labels[components.LabelKey])
-		componentTypeB := components.MustGetComponentType(b.Labels[components.LabelKey])
-		return int(componentTypeA) - int(componentTypeB)
+		componentTypeA := components.MustParseComponentType(a.Labels[components.LabelKey])
+		componentTypeB := components.MustParseComponentType(b.Labels[components.LabelKey])
+		return components.ADifferenceFromB(componentTypeA, componentTypeB)
 	})
 
 	pipeline, err := generatePipeline(basePipeline, taskList)
@@ -172,7 +172,7 @@ func generatePipeline(pipeline *tektonv1beta1api.Pipeline, taskList []*tektonv1b
 
 	for _, task := range taskList {
 		componentTypeStr := task.Labels[components.LabelKey]
-		componentType, err := components.ToComponentType(componentTypeStr)
+		componentType, err := components.ParseComponentType(componentTypeStr)
 		if err != nil {
 			return nil, errors.Errorf("%s: task has invalid component type: %w", task.Name, err)
 		}
@@ -221,7 +221,7 @@ func generatePipeline(pipeline *tektonv1beta1api.Pipeline, taskList []*tektonv1b
 			}
 
 			if param.Name == "anchors" {
-				anchorTargetComponentType := componentType - 1
+				anchorTargetComponentType := components.GetPrevious(componentType)
 				values := []string{}
 
 				// get all the tasks that should be finished before this one starts
