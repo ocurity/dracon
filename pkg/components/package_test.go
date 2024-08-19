785bc71776ca59ad00b4e4fc1a0622b665ffa086
sg <spyros@ocurity.com>
2024-08-19 21:34:37 +0100

    iclose #307 by making addanchors and add env vars idempotent

--- pkg/components/package_test.go
+++ pkg/components/package_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"slices"
 	"strings"
 	"testing"
 
@@ -113,5 +114,104 @@ version: %s
 		}
 
 		require.Equal(t, "anchor", task.Spec.Steps[len(task.Spec.Steps)-1].Name, "task %s has no anchor step", task.Name)
+	}
+}
+
+func TestProcessTasksAnchorsEnvExist(t *testing.T) {
+	taskList, err := LoadTasks(
+		context.Background(),
+		[]string{
+			"testdata/base/task.yaml",
+			"testdata/sources/git/task.yaml",
+			"testdata/producers/cdxgen/task.yaml",
+			"testdata/producers/aggregator/task.yaml",
+			"testdata/enrichers/aggregator/task.yaml",
+			"testdata/consumers/arangodb/task.yaml",
+		},
+	)
+	require.NoError(t, err)
+
+	// add anchors and env vars once
+	require.NoError(t, ProcessTasks(taskList...))
+
+	// ensure anchors have been added once
+	requireAnchors(t, taskList)
+
+	// ensure envvars have been added once
+	requireEnvVars(t, taskList)
+
+	// add anchors and env vars again
+	require.NoError(t, ProcessTasks(taskList...))
+
+	// ensure anchors have been added once
+	requireAnchors(t, taskList)
+	requireEnvVars(t, taskList)
+}
+
+//revive:disable:cyclomatic High complexity score but easy to understand
+//revive:disable:cognitive-complexity High complexity score but easy to understand
+func requireAnchors(t *testing.T, taskList []*tektonv1beta1api.Task) {
+	for _, task := range taskList {
+		require.NotEmpty(t, task.Spec.Steps)
+		if strings.Contains(task.Name, "base") || strings.Contains(task.Name, "git") { // base and git tasks have no anchors
+			continue
+		}
+
+		anchorParamNames := []string{}
+		for _, param := range task.Spec.Params {
+			if param.Name == "anchors" {
+				anchorParamNames = append(anchorParamNames, "anchors")
+			}
+		}
+
+		anchorNames := []string{}
+		for _, step := range task.Spec.Steps {
+			if step.Name == "anchor" {
+				anchorNames = append(anchorNames, "anchor")
+			}
+		}
+		anchorResultNames := []string{}
+		for _, result := range task.Spec.Results {
+			if result.Name == "anchor" {
+				anchorResultNames = append(anchorResultNames, "anchor")
+			}
+		}
+		require.Equal(t, len(anchorParamNames), 1)
+
+		if strings.Contains(task.Name, "consumer") {
+			continue // consumers have no anchor steps
+		}
+		require.Equal(t, len(anchorNames), 1)
+		require.Equal(t, len(anchorResultNames), 1)
+	}
+}
+
+//revive:disable:cyclomatic High complexity score but easy to understand
+//revive:disable:cognitive-complexity High complexity score but easy to understand
+func requireEnvVars(t *testing.T, taskList []*tektonv1beta1api.Task) {
+	envVars := []string{"DRACON_SCAN_TIME", "DRACON_SCAN_ID", "DRACON_SCAN_TAGS"}
+	params := []string{"dracon_scan_id", "dracon_scan_start_time", "dracon_scan_tags"}
+	for _, task := range taskList {
+		taskParams := []string{}
+		if !strings.Contains(task.Name, "producer") || strings.Contains(task.Name, "aggregator") { // env vars are for producers only
+			continue
+		}
+		for _, p := range task.Spec.Params {
+			if slices.Contains(params, p.Name) {
+				taskParams = append(taskParams, p.Name)
+			}
+		}
+
+		require.Equalf(t, len(taskParams), len(params), "task %s has missing params", task.Name)
+		for _, step := range task.Spec.Steps {
+			require.NotEmptyf(t, step.Env, "task %s has no env vars", task.Name)
+			stepEnvVars := []string{}
+			for _, env := range step.Env {
+				if slices.Contains(envVars, env.Name) {
+					stepEnvVars = append(stepEnvVars, env.Name)
+				}
+			}
+			require.Equalf(t, len(envVars), len(stepEnvVars), "task %s has env vars %#v", task.Name, stepEnvVars)
+		}
 	}
 }
