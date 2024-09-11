ffdbfd30453354e6c071bef66fd3a33d657cdf16
andream16 <andrea@ocurity.com>
2024-09-11 15:17:06 +0100

    introduce multi-platform builds #334
    This commit extends the base Dockerfile so that it can copy a binary
    from an arbitrary path and copy it into a target one inside the
    filesystem of the container image.
    The scripts building the component binaries and containers have also
    been extended with environment variables to help them build the binaries
    for the expected architecture. The Makefile has acquired also the same
    environment variable which are set by default in the host's
    architecture. In the case of the `dev-deploy` target, the architecture
    is set by default to `linux/amd64` since containers are usually executed
    on linux/amd64 systems or on linux/amd64 VMs running the Docker daemon
    (Windows, some Macs) or on VMs running linux/arm64 which supports amd64
    binaries out of the box.

--- scripts/build_component_container.sh
+++ scripts/build_component_container.sh
@@ -4,36 +4,45 @@ set -e;
 
 source ./scripts/util.sh
 
+# Sanity check for not arguments being passed.
 if [ "$#" -eq 0 ]
 then
-    util::error "No directory provided to build"
+    util::error "No directory argument provided to build."
     exit 1
 fi
 
-executable=$(basename $(dirname "${1}"))
+dir_name="$1"
+GOOS="${GOOS}"
+GOARCH="${GOARCH}"
+build_architecture="${GOOS}/${GOARCH}"
 
-echo "${1}" | grep -Eq ^components/producers/.*$ && executable="${executable}-parser" || true
-echo "${1}" | grep -Eq ^components/enrichers/.*$ && executable="${executable}" || true
-echo "${1}" | grep -Eq ^components/consumers/.*$ && executable="${executable}" || true
+executable=$(basename $(dirname ${dir_name}))
 
-executable_src_path=$(dirname "${1}")
-executable_path=$(dirname "${1}")/"${executable}"
+echo ${dir_name} | grep -Eq ^components/producers/.*$ && executable="${executable}-parser" || true
+echo ${dir_name} | grep -Eq ^components/enrichers/.*$ && executable="${executable}" || true
+echo ${dir_name} | grep -Eq ^components/consumers/.*$ && executable="${executable}" || true
+
+EXECUTABLE_SRC_PATH="$(dirname ${dir_name})/${build_architecture}/${executable}"
+COMPONENT_PATH="$(dirname ${dir_name})"
+EXECUTABLE_TARGET_PATH="${COMPONENT_PATH}/${executable}"
 
 BASE_IMAGE_PATH=$(realpath ${BASE_IMAGE_PATH:-./containers/Dockerfile.base})
 
-if make -C "${executable_src_path}" --no-print-directory --dry-run container >/dev/null 2>&1
+if make -C "${COMPONENT_PATH}" --no-print-directory --dry-run container >/dev/null 2>&1
 then
-    make -C "${executable_src_path}" --no-print-directory --quiet container BASE_IMAGE_PATH="${BASE_IMAGE_PATH}" CONTAINER_REPO="${CONTAINER_REPO}" DRACON_VERSION="${DRACON_VERSION}"
+    make -C "${COMPONENT_PATH}" --no-print-directory --quiet container BASE_IMAGE_PATH="${BASE_IMAGE_PATH}" CONTAINER_REPO="${CONTAINER_REPO}" DRACON_VERSION="${DRACON_VERSION}" BUILD_ARCHITECTURE="${build_architecture}"
 else
     docker build \
-        --build-arg EXECUTABLE_SRC_PATH=${executable_path} \
-        --build-arg EXECUTABLE_TARGET_PATH=${executable_path} \
-        -t "${CONTAINER_REPO}/${executable_src_path}:${DRACON_VERSION}" \
+        --build-arg EXECUTABLE_SRC_PATH="${EXECUTABLE_SRC_PATH}" \
+        --build-arg EXECUTABLE_TARGET_PATH="${EXECUTABLE_TARGET_PATH}" \
+        --tag "${CONTAINER_REPO}/${COMPONENT_PATH}:${DRACON_VERSION}" \
         $([ "${SOURCE_CODE_REPO}" != "" ] && echo "--label=org.opencontainers.image.source=${SOURCE_CODE_REPO}" ) \
-        -f "${BASE_IMAGE_PATH}" ./bin
+        --file "${BASE_IMAGE_PATH}" \
+        --platform "${build_architecture}" \
+        ./bin
 fi
 
-if make -C "${executable_src_path}" --no-print-directory --dry-run extras >/dev/null 2>&1
+if make -C "${COMPONENT_PATH}" --no-print-directory --dry-run extras >/dev/null 2>&1
 then
-    make -C "${executable_src_path}" --no-print-directory --quiet extras CONTAINER_REPO="${CONTAINER_REPO}" DRACON_VERSION="${DRACON_VERSION}"
+    make -C "${COMPONENT_PATH}" --no-print-directory --quiet extras CONTAINER_REPO="${CONTAINER_REPO}" DRACON_VERSION="${DRACON_VERSION}"
 fi
