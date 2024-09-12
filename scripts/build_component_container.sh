208bcafed6736a7d354576bf0e672a7f03ab8898
sg <spyros@ocurity.com>
2024-09-12 20:24:00 +0100

    make build component containers script build using a base dockerfile that has certificates

--- scripts/build_component_container.sh
+++ scripts/build_component_container.sh
@@ -19,20 +19,18 @@ echo "${1}" | grep -Eq ^components/consumers/.*$ && executable="${executable}" |
 executable_src_path=$(dirname "${1}")
 executable_path=$(dirname "${1}")/"${executable}"
 
+BASE_IMAGE_PATH=$(realpath ${BASE_IMAGE_PATH:-./containers/Dockerfile.base})
+
 if make -C "${executable_src_path}" --no-print-directory --dry-run container >/dev/null 2>&1
 then
-    make -C "${executable_src_path}" --no-print-directory --quiet container CONTAINER_REPO="${CONTAINER_REPO}" DRACON_VERSION="${DRACON_VERSION}"
+    make -C "${executable_src_path}" --no-print-directory --quiet container BASE_IMAGE_PATH="${BASE_IMAGE_PATH}" CONTAINER_REPO="${CONTAINER_REPO}" DRACON_VERSION="${DRACON_VERSION}"
 else
-    dockerfile_template="
-        FROM ${BASE_IMAGE:-scratch}                     \n
-        COPY ${executable_path} /app/${executable_path} \n
-        ENTRYPOINT [\"/app/${executable_path}\"]        \n
-    "
-    dockerfile_path=$(mktemp)
-    printf "${dockerfile_template}" > "${dockerfile_path}"
-    docker --debug build -t "${CONTAINER_REPO}/${executable_src_path}:${DRACON_VERSION}" \
+    docker build \
+        --build-arg EXECUTABLE_SRC_PATH=${executable_path} \
+        --build-arg EXECUTABLE_TARGET_PATH=${executable_path} \
+        -t "${CONTAINER_REPO}/${executable_src_path}:${DRACON_VERSION}" \
         $([ "${SOURCE_CODE_REPO}" != "" ] && echo "--label=org.opencontainers.image.source=${SOURCE_CODE_REPO}" ) \
-        -f "${dockerfile_path}" ./bin
+        -f "${BASE_IMAGE_PATH}" ./bin
 fi
 
 if make -C "${executable_src_path}" --no-print-directory --dry-run extras >/dev/null 2>&1
