ce04ecc71cc900e3e8b9b3cf60eeb116223b0160
andream16 <andrea@ocurity.com>
2024-09-13 15:47:03 +0100

    debugging

--- scripts/build_component_container.sh
+++ scripts/build_component_container.sh
@@ -3,7 +3,6 @@
 set -e;
 
 source ./scripts/util.sh
-containers_path=./containers
 
 if [ "$#" -eq 0 ]
 then
@@ -17,32 +16,23 @@ echo "${1}" | grep -Eq ^components/producers/.*$ && executable="${executable}-pa
 echo "${1}" | grep -Eq ^components/enrichers/.*$ && executable="${executable}" || true
 echo "${1}" | grep -Eq ^components/consumers/.*$ && executable="${executable}" || true
 
+container_platform=${GOOS}/${GOARCH}
 executable_src_path=$(dirname "${1}")
-executable_path=$(dirname "${1}")/"${executable}"
+executable_path=$(dirname "${1}")/"${container_platform}/${executable}"
+
+BASE_IMAGE_PATH=$(realpath ${BASE_IMAGE_PATH:-./containers/Dockerfile.base})
 
 if make -C "${executable_src_path}" --no-print-directory --dry-run container >/dev/null 2>&1
 then
-    make -C "${executable_src_path}" --no-print-directory --quiet container CONTAINER_REPO="${CONTAINER_REPO}" DRACON_VERSION="${DRACON_VERSION}"
+    make -C "${executable_src_path}" --no-print-directory --quiet container BASE_IMAGE_PATH="${BASE_IMAGE_PATH}" CONTAINER_REPO="${CONTAINER_REPO}" DRACON_VERSION="${DRACON_VERSION}"
 else
-    dockerfile_template=""
-    if [[ -n "${BASE_IMAGE+x}" ]]
-    then
-        echo "Using base image: ${BASE_IMAGE}"
-        dockerfile_template=$(cat "${containers_path}/Dockerfile.base.image")
-    else
-        BASE_IMAGE=''
-        dockerfile_template=$(cat "${containers_path}/Dockerfile.base")
-    fi
-
-    dockerfile_path=$(mktemp)
-    printf "${dockerfile_template}" > "${dockerfile_path}"
     docker build \
-        --platform "${GOOS}/${GOARCH}" \
-        --build-arg EXECUTABLE_PATH=${executable_path} \
-        --build-arg BASE_IMAGE=${BASE_IMAGE} \
+        --platform "${container_platform}" \
+        --build-arg EXECUTABLE_SRC_PATH=${executable_path} \
+        --build-arg EXECUTABLE_TARGET_PATH=${executable_path} \
         -t "${CONTAINER_REPO}/${executable_src_path}:${DRACON_VERSION}" \
         $([ "${SOURCE_CODE_REPO}" != "" ] && echo "--label=org.opencontainers.image.source=${SOURCE_CODE_REPO}" ) \
-        -f "${dockerfile_path}" ./bin
+        -f "${BASE_IMAGE_PATH}" ./bin
 fi
 
 if make -C "${executable_src_path}" --no-print-directory --dry-run extras >/dev/null 2>&1
