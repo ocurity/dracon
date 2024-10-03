56a49eda59b5d298eda2291a5a282df89d786d00
sg <spyros@ocurity.com>
2024-10-03 17:01:41 +0100

    implement #394 - add a custom key-value pair enricher

---
+++ components/enrichers/custom-annotation/README.md
@@ -0,0 +1,5 @@
+# Custom Annotation Enricher
+
+This enricher adds a set of custom annotations to every finding
+Useful for development purposes and for tagging results
+Can be used multiple times to add multiple annotations
--- components/enrichers/test_utils.go
+++ components/enrichers/test_utils.go
@@ -33,3 +33,79 @@ func GetEmptyLaunchToolResponse(_ *testing.T) []*draconv1.LaunchToolResponse {
 		},
 	}
 }
+
+// GetEmptyLaunchToolResponse returns a slice of LaunchToolResponse with no issues
+func GetLaunchToolResponse(_ *testing.T) []*draconv1.LaunchToolResponse {
+	code := `this
+					is
+					some
+					code`
+	return []*draconv1.LaunchToolResponse{
+		{
+			ToolName: "tool1",
+			Issues: []*draconv1.Issue{
+				{
+					Target:         "file:/a/b/c/d.php:1-2",
+					Type:           "sometype",
+					Title:          "this is a title",
+					Severity:       draconv1.Severity_SEVERITY_CRITICAL,
+					Cvss:           1.0,
+					Confidence:     draconv1.Confidence_CONFIDENCE_CRITICAL,
+					Description:    "this is a handy dandy description",
+					Source:         "this is a source",
+					Cve:            "CVE-2020-123",
+					Uuid:           "d9681ae9-223b-4df8-a422-7b29bb917a36",
+					Cwe:            []int32{123},
+					ContextSegment: &code,
+				},
+				{
+					Target:         "file:/a/b/c/d.go:2-3",
+					Type:           "sometype1",
+					Title:          "this is a title1",
+					Severity:       draconv1.Severity_SEVERITY_CRITICAL,
+					Cvss:           1.0,
+					Confidence:     draconv1.Confidence_CONFIDENCE_CRITICAL,
+					Description:    "this is a handy dandy description1",
+					Source:         "this is a source1",
+					Cve:            "CVE-2020-124",
+					Uuid:           "a9681ae9-223b-4df8-a422-7b29bb917a36",
+					Cwe:            []int32{123},
+					ContextSegment: &code,
+				},
+			},
+		},
+		{
+			ToolName: "tool2",
+			Issues: []*draconv1.Issue{
+				{
+					Target:         "file:/a/b/c/d.py:1-2",
+					Type:           "sometype",
+					Title:          "this is a title",
+					Severity:       draconv1.Severity_SEVERITY_CRITICAL,
+					Cvss:           1.0,
+					Confidence:     draconv1.Confidence_CONFIDENCE_CRITICAL,
+					Description:    "this is a handy dandy description",
+					Source:         "this is a source",
+					Cve:            "CVE-2020-123",
+					Uuid:           "q9681ae9-223b-4df8-a422-7b29bb917a36",
+					Cwe:            []int32{123},
+					ContextSegment: &code,
+				},
+				{
+					Target:         "file:/a/b/c/d.py:2-3",
+					Type:           "sometype1",
+					Title:          "this is a title1",
+					Severity:       draconv1.Severity_SEVERITY_CRITICAL,
+					Cvss:           1.0,
+					Confidence:     draconv1.Confidence_CONFIDENCE_CRITICAL,
+					Description:    "this is a handy dandy description1",
+					Source:         "this is a source1",
+					Cve:            "CVE-2020-124",
+					Uuid:           "w9681ae9-223b-4df8-a422-7b29bb917a36",
+					Cwe:            []int32{123},
+					ContextSegment: &code,
+				},
+			},
+		},
+	}
+}
